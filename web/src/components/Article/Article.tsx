0f8edbf77f41eb0a76c76d4769ada744f09d18be
Michael Sharpe <m.sharpe@project89.org>
2022-11-24 15:24:47 -0800

    Truncate articles

--- web/src/components/Article/Article.tsx
+++ web/src/components/Article/Article.tsx
@@ -2,19 +2,25 @@ import type { Post } from 'types/graphql'
 
 import { Link, routes } from '@redwoodjs/router'
 
+const truncate = (text: string, length: number) => {
+  return text.substring(0, length) + '...'
+}
 interface Props {
-  article: Post
+  article: Omit<Post, 'createdAt'>
+  summary?: boolean
 }
 
-const Article = ({ article }: Props) => {
+const Article = ({ article, summary = false }: Props) => {
   return (
     <article>
       <header>
-        <h2 className="text-xl text-blue-700 font-semibold">
+        <h2 className="text-xl font-semibold text-blue-700">
           <Link to={routes.article({ id: article.id })}>{article.title}</Link>
         </h2>
       </header>
-      <div className="mt-2 text-gray-900 font-light">{article.body}</div>
+      <div className="mt-2 font-light text-gray-900">
+        {summary ? truncate(article.body, 100) : article.body}
+      </div>
     </article>
   )
 }
